83626570fbd0928399eddfa9316e1f273f5785d9
30Dennis <otienodennis69@gmail.com>
2023-08-23 10:51:33 +0300

    Created a sign up page

--- app/src/main/java/com/example/weruapp/SignUp.kt
+++ app/src/main/java/com/example/weruapp/SignUp.kt
@@ -1,24 +1,23 @@
 package com.example.weruapp
 
-import android.graphics.Paint.Style
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Spacer
-
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -28,64 +27,73 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.example.weruapp.ui.theme.WeruappTheme
 
-class LoginActivity : ComponentActivity() {
+class SignUp : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-           loginInterface()
-            }
+          signUpDisplay()
         }
     }
-
+}
 @OptIn(ExperimentalMaterial3Api::class)
 @Preview(showBackground = true)
-
 @Composable
 
-
-
-
-fun loginInterface(){
+fun signUpDisplay(){
     var username by remember {
         mutableStateOf("")
     }
     var password by remember {
         mutableStateOf("")
     }
-    Column(
-        modifier = Modifier
-            .background(Color.LightGray)
-            .fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
+ Column(
+     modifier = Modifier
+         .background(color = Color.Gray)
+         .fillMaxSize()
+     ,
+     verticalArrangement = Arrangement.Center,
+     horizontalAlignment = Alignment.CenterHorizontally
 
-    ) {
-      Text(text = "Welcome back, please login", color = Color.White, fontSize = 15.sp, fontWeight = FontWeight.Bold, fontFamily = FontFamily.Monospace)
-      OutlinedTextField(value = username,
-          onValueChange ={username} ,
-          label = { Text(text = "Username")},
-          modifier = Modifier.fillMaxWidth()
-          )
-        Spacer(modifier = Modifier.height(10.dp))
-      OutlinedTextField(value = password,
-            onValueChange ={password} ,
-            label = { Text(text = "password")},
-            modifier = Modifier.fillMaxWidth()
-        )
-        Spacer(modifier = Modifier.height(20.dp))
+ ) {
+     Text(text = "Sign up ",
+          fontFamily = FontFamily.Monospace,
+         fontSize = 30.sp,
+         textAlign = TextAlign.Center,
+         )
+     Column(
+       modifier = Modifier
+           .background(color = Color.LightGray)
+           .padding(all = 20.dp),
+       horizontalAlignment = Alignment.CenterHorizontally
 
-        Button(onClick = { /*TODO*/ },
-                     modifier = Modifier.fillMaxWidth()
+     ) {
+
+         OutlinedTextField(value = username, onValueChange ={username},
+             label = { Text(text = "Username")},
+             modifier = Modifier.fillMaxWidth()
+         )
+         Spacer(modifier = Modifier.height(20.dp))
+         OutlinedTextField(value = password, onValueChange ={password},
+             label = { Text(text = "password")},
+             modifier = Modifier.fillMaxWidth()
+         )
+         Spacer(modifier = Modifier.height(20.dp))
+         Button(onClick = { /*TODO*/ }, modifier = Modifier.fillMaxWidth() ) {
+             Text(text = "SIGN UP")
+         }
+     }
 
 
-            ) {
-           Text(text = "LOGIN")
-        }
-    }
+ }
 }
+
+
+
+
+
